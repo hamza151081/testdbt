7ad5fe8140fedd6264e0be7332d1f0d33567b343
hamza.moubareckou@gmail.com <hamza.moubareckou@gmail.com>
2024-03-09 14:18:13 +0000

    changing the nature of second table

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,4 @@
-
 -- Use the `ref` function to select from other models
+{{ config(materialized='view') }}
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+select * from {{ ref("my_first_dbt_model") }} where id = 1
